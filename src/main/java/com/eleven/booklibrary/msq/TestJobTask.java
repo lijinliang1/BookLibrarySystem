59d7c584528f23c1a5b59b64af765d4d05854af0
lijinliang <15117963494@139.com>
2018-06-06 08:47:54 +0800

    添加定时器quartz

---
+++ src/main/java/com/eleven/booklibrary/msq/TestJobTask.java
@@ -0,0 +1,18 @@
+package com.eleven.booklibrary.msq;   
+/**
+* 创建时间：2018年6月5日 下午7:13:13
+* 项目名称：BookLibrarySystem
+* @author lijinliang
+* @version 1.0
+* 文件名称：TestJobTask.java
+* 类说明：
+*/
+public class TestJobTask {
+	 /** 
+     *业务逻辑处理 
+     */  
+      public void   service(){  
+          /**业务逻辑*/  
+              System.out.println(System.currentTimeMillis());
+      }  
+}
